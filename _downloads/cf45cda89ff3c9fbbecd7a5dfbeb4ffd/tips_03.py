cdf8b1fb5183d7275e856bdf40625826287460a5
dipinknair <26918585+dipinknair@users.noreply.github.com>
2024-10-07 15:22:09 +0000

    Deploying to gh-pages from @ ansys/pymechanical-embedding-examples@83ef0fdf984f3bdc2a046c3206510f9bd3afbe3d 🚀

---
+++ _downloads/cf45cda89ff3c9fbbecd7a5dfbeb4ffd/tips_03.py
@@ -0,0 +1,54 @@
+""" .. _ref_tips_03:
+
+Project tree
+--------------------
+
+Display the heirarchial Mechanical project structure.
+"""
+
+# %%
+# Import necessary libraries
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+import ansys.mechanical.core as mech
+from ansys.mechanical.core.examples import delete_downloads, download_file
+
+# %%
+# Embed Mechanical and set global variables
+
+app = mech.App(version=242)
+app.update_globals(globals())
+print(app)
+
+
+# %%
+# Download the mechdb file
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+mechdb_path = download_file("graphics_test.mechdb", "pymechanical", "test_files")
+
+# %%
+# Load the mechdb file inside Mechanical
+
+app.open(mechdb_path)
+
+# %%
+# Display the project tree
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+app.print_tree()
+
+# %%
+# Display the tree only under the first analysis
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+app.print_tree(Model.Analyses[0])
+
+
+# %%
+# Cleanup
+# ~~~~~~~
+
+delete_downloads()
+app.new()
