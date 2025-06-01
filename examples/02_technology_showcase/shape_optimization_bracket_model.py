5c3b1653892074d974e9ade0d4fc8bf12d1efd41
dkunhamb <dipin.kunhambunair@ansys.com>
2025-06-01 11:38:29 -0500

    update with reformatting

--- examples/02_technology_showcase/shape_optimization_bracket_model.py
+++ examples/02_technology_showcase/shape_optimization_bracket_model.py
@@ -32,33 +32,70 @@ Scripts then evaluate the following results: deformation and
 optimized shape.
 """
 
-# %%
-# Import necessary libraries
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+## %%
+# Import the necessary libraries
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-import os
+from pathlib import Path
+from typing import TYPE_CHECKING
 
 from ansys.mechanical.core import App
 from ansys.mechanical.core.examples import delete_downloads, download_file
 from matplotlib import image as mpimg
 from matplotlib import pyplot as plt
 
-# %%
-# Embed mechanical and set global variables
+if TYPE_CHECKING:
+    import Ansys
 
-app = App()
-app.update_globals(globals())
+# %%
+# Initialize the embedded application
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+app = App(globals=globals())
 print(app)
 
-cwd = os.path.join(os.getcwd(), "out")
+# %%
+# Create functions to set camera and display images
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Set the path for the output files (images, gifs, mechdat)
+output_path = Path.cwd() / "out"
 
 
-def display_image(image_name):
-    plt.figure(figsize=(16, 9))
-    plt.imshow(mpimg.imread(os.path.join(cwd, image_name)))
-    plt.xticks([])
-    plt.yticks([])
-    plt.axis("off")
+def display_image(
+    image_path: str,
+    pyplot_figsize_coordinates: tuple = (16, 9),
+    plot_xticks: list = [],
+    plot_yticks: list = [],
+    plot_axis: str = "off",
+) -> None:
+    """Display the image with the specified parameters.
+
+    Parameters
+    ----------
+    image_path : str
+        The path to the image file to display.
+    pyplot_figsize_coordinates : tuple
+        The size of the figure in inches (width, height).
+    plot_xticks : list
+        The x-ticks to display on the plot.
+    plot_yticks : list
+        The y-ticks to display on the plot.
+    plot_axis : str
+        The axis visibility setting ('on' or 'off').
+    """
+    # Set the figure size based on the coordinates specified
+    plt.figure(figsize=pyplot_figsize_coordinates)
+    # Read the image from the file into an array
+    image_path = str(output_path / image_path)
+    plt.imshow(mpimg.imread(image_path))
+    # Get or set the current tick locations and labels of the x-axis
+    plt.xticks(plot_xticks)
+    # Get or set the current tick locations and labels of the y-axis
+    plt.yticks(plot_yticks)
+    # Turn off the axis
+    plt.axis(plot_axis)
+    # Display the figure
     plt.show()
 
 
@@ -66,40 +103,61 @@ def display_image(image_name):
 # Configure graphics for image export
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Graphics.Camera.SetSpecificViewOrientation(ViewOrientationType.Iso)
-Graphics.Camera.SetFit()
+# Define the graphics and camera
+graphics = app.Graphics
+camera = graphics.Camera
+
+# Set the camera orientation to the isometric view and set the camera to fit the model
+camera.SetSpecificViewOrientation(ViewOrientationType.Iso)
+camera.SetFit()
+
+# Set the image export format and settings
 image_export_format = GraphicsImageExportFormat.PNG
 settings_720p = Ansys.Mechanical.Graphics.GraphicsImageExportSettings()
-settings_720p.Resolution = GraphicsResolutionType.EnhancedResolution
-settings_720p.Background = GraphicsBackgroundType.White
+settings_720p.Resolution = (
+    Ansys.Mechanical.DataModel.Enums.GraphicsResolutionType.EnhancedResolution
+)
+settings_720p.Background = Ansys.Mechanical.DataModel.Enums.GraphicsBackgroundType.White
 settings_720p.Width = 1280
 settings_720p.Height = 720
 settings_720p.CurrentGraphicsDisplay = False
-Graphics.Camera.Rotate(180, CameraAxisType.ScreenY)
 
 # %%
-# Download and import geometry
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Download the required files
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # Download the geometry file
-
 geometry_path = download_file("bracket_model.agdb", "pymechanical", "embedding")
-geometry_import_group_11 = Model.GeometryImportGroup
-geometry_import = geometry_import_group_11.AddGeometryImport()
 
+# %%
+# Import the geometry
+# ~~~~~~~~~~~~~~~~~~~
+
+# Define the model
+model = app.Model
+
+# Add the geometry import to the geometry import group
+geometry_import_group = model.GeometryImportGroup
+geometry_import = geometry_import_group.AddGeometryImport()
+
+# Set the geometry import format and settings
 geometry_import_format = (
     Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic
 )
 geometry_import_preferences = Ansys.ACT.Mechanical.Utilities.GeometryImportPreferences()
 geometry_import_preferences.ProcessNamedSelections = True
+geometry_import_preferences.NamedSelectionKey = ""
+geometry_import_preferences.ProcessMaterialProperties = True
+geometry_import_preferences.ProcessCoordinateSystems = True
+
+# Import the geometry with the specified settings
 geometry_import.Import(
     geometry_path, geometry_import_format, geometry_import_preferences
 )
 
-# sphinx_gallery_start_ignore
-assert str(geometry_import.ObjectState) == "Solved", "Geometry Import unsuccessful"
-# sphinx_gallery_end_ignore
-
+# Visualize the model in 3D
 app.plot()
+
 
 # %%
 # Define Named Selections
@@ -117,11 +175,12 @@ EXCLUSON_REGION_NS = [
 BRACKET_NS = [x for x in ExtAPI.DataModel.Tree.AllObjects if x.Name == "bracket"][0]
 
 # %%
-# Mesh
-# ~~~~
+# Define the mesh settings and generate the mesh
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-mesh_13 = Model.Mesh
-automatic_method_33 = mesh_13.AddAutomaticMethod()
+
+mesh = app.Model.Mesh
+automatic_method_33 = mesh.AddAutomaticMethod()
 automatic_method_33.ScopingMethod = GeometryDefineByType.Component
 
 selection = NS_GRP.Children[3]
@@ -129,79 +188,82 @@ automatic_method_33.Location = selection
 automatic_method_33.Method = MethodType.AllTriAllTet
 automatic_method_33.ElementOrder = ElementOrder.Linear
 
-sizing_35 = mesh_13.AddSizing()
+sizing_35 = mesh.AddSizing()
 sizing_35.ScopingMethod = GeometryDefineByType.Component
 selection = NS_GRP.Children[3]
 sizing_35.Location = selection
 sizing_35.ElementSize = Quantity(6e-3, "m")
 
-mesh_13.GenerateMesh()
+mesh.GenerateMesh()
+
+# Display mesh
+
+app.Tree.Activate([mesh])
+camera.SetFit()
+graphics.ExportImage(str(output_path / "mesh.png"), image_export_format, settings_720p)
+display_image("mesh.png")
 
 # %%
 # Define Analysis
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Add Structural analysis
 
-model_10 = Model
-analysis_51 = model_10.AddStaticStructuralAnalysis()
+model = app.Model
+static_structural_analysis = model.AddStaticStructuralAnalysis()
 
 # %%
 # Define loads and boundary conditions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-fixed_support_55 = analysis_51.AddFixedSupport()
+fixed_support = static_structural_analysis.AddFixedSupport()
 selection = NS_GRP.Children[0]
-fixed_support_55.Location = selection
+fixed_support.Location = selection
 
-force_57 = analysis_51.AddForce()
+force = static_structural_analysis.AddForce()
 selection = NS_GRP.Children[1]
-force_57.Location = selection
-force_57.DefineBy = LoadDefineBy.Components
-force_57.ZComponent.Output.SetDiscreteValue(0, Quantity(25000, "N"))
+force.Location = selection
+force.DefineBy = LoadDefineBy.Components
+force.ZComponent.Output.SetDiscreteValue(0, Quantity(25000, "N"))
 
 # %%
 # Analysis settings
 # ~~~~~~~~~~~~~~~~~
 
-STAT_STRUC = Model.Analyses[0]
-SOLN = STAT_STRUC.Solution
+solution = static_structural_analysis.Solution
 
 # %%
 # Insert results
 # ~~~~~~~~~~~~~~
 
-Total_Deformation = SOLN.AddTotalDeformation()
+total_deformation = solution.AddTotalDeformation()
 
 # %%
 # Solve
 # ~~~~~
 
-SOLN.Solve(True)
-STAT_STRUC_SS = SOLN.Status
+solution.Solve(True)
+solution_status = solution.Status
 # sphinx_gallery_start_ignore
-assert str(STAT_STRUC_SS) == "Done", "Solution status is not 'Done'"
+assert str(solution_status) == "Done", "Solution status is not 'Done'"
 # sphinx_gallery_end_ignore
 
 # %%
-# Messages
-# ~~~~~~~~
+# Show messages
+# ~~~~~~~~~~~~~
 
-Messages = ExtAPI.Application.Messages
-if Messages:
-    for message in Messages:
-        print(f"[{message.Severity}] {message.DisplayString}")
-else:
-    print("No [Info]/[Warning]/[Error] Messages")
+# Print all messages from Mechanical
+app.messages.show()
+
 
 # %%
 # Results
 # ~~~~~~~
 # Total deformation
 
-Tree.Activate([Total_Deformation])
-Graphics.Camera.SetFit()
-Graphics.ExportImage(
-    os.path.join(cwd, "total_deformation.png"), image_export_format, settings_720p
+app.Tree.Activate([total_deformation])
+camera.SetFit()
+graphics.ExportImage(
+    str(output_path / "total_deformation.png"), image_export_format, settings_720p
 )
 display_image("total_deformation.png")
 
@@ -210,93 +272,88 @@ display_image("total_deformation.png")
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Add Topology Optimization Analysis
 
-analysis_61 = model_10.AddTopologyOptimizationAnalysis()
-analysis_61.ImportLoad(Model.Analyses[0])
+topology_optimization = model.AddTopologyOptimizationAnalysis()
+topology_optimization.ImportLoad(static_structural_analysis)
 
 # %%
 # Define Optimization Settings
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Specify the shape optimization region
 
-optimization_region_64 = DataModel.GetObjectsByType(
+optimization_region = DataModel.GetObjectsByType(
     DataModelObjectCategory.OptimizationRegion
 )[0]
 selection = NS_GRP.Children[3]
-optimization_region_64.DesignRegionLocation = selection
-optimization_region_64.ExclusionScopingMethod = GeometryDefineByType.Component
+optimization_region.DesignRegionLocation = selection
+optimization_region.ExclusionScopingMethod = GeometryDefineByType.Component
 selection = NS_GRP.Children[2]
-optimization_region_64.ExclusionRegionLocation = selection
+optimization_region.ExclusionRegionLocation = selection
 
-optimization_region_64.OptimizationType = OptimizationType.Shape
-optimization_region_64.ShapeMoveLimitControl = TopoPropertyControlType.Manual
-optimization_region_64.MorphingIterationMoveLimit = 0.002
-optimization_region_64.MaxCumulatedDisplacementControl = TopoPropertyControlType.Manual
-optimization_region_64.MorphingTotalMoveLimit = 0.02
-optimization_region_64.MeshDeformationToleranceControl = TopoPropertyControlType.Manual
+optimization_region.OptimizationType = OptimizationType.Shape
+optimization_region.ShapeMoveLimitControl = TopoPropertyControlType.Manual
+optimization_region.MorphingIterationMoveLimit = 0.002
+optimization_region.MaxCumulatedDisplacementControl = TopoPropertyControlType.Manual
+optimization_region.MorphingTotalMoveLimit = 0.02
+optimization_region.MeshDeformationToleranceControl = TopoPropertyControlType.Manual
 
 # %%
 # Define Objective
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Specify objective as minimizing volume
 
-Objective_type = DataModel.GetObjectsByType(DataModelObjectCategory.Objective)[0]
-Objective_type.Worksheet.SetObjectiveType(0, ObjectiveType.MinimizeVolume)
+objective_type = DataModel.GetObjectsByType(DataModelObjectCategory.Objective)[0]
+objective_type.Worksheet.SetObjectiveType(0, ObjectiveType.MinimizeVolume)
 
 # %%
 # Define Compliance Settings
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Specify compliance as response constraint
 
-compliance_constraint_75 = analysis_61.AddComplianceConstraint()
-compliance_constraint_75.ComplianceLimit.Output.SetDiscreteValue(0, Quantity(0.27, "J"))
+compliance_constraint = topology_optimization.AddComplianceConstraint()
+compliance_constraint.ComplianceLimit.Output.SetDiscreteValue(0, Quantity(0.27, "J"))
 
-mass_constraint_67 = DataModel.GetObjectsByName("Response Constraint")
-DataModel.Remove(mass_constraint_67)
+mass_constraint = DataModel.GetObjectsByName("Response Constraint")
+DataModel.Remove(mass_constraint)
 
 # %%
 # Analysis settings
 # ~~~~~~~~~~~~~~~~~
-STAT_SHAPE = Model.Analyses[1]
-SOLN = STAT_SHAPE.Solution
+topo_solution = topology_optimization.Solution
 
 # %%
 # Insert results
 # ~~~~~~~~~~~~~~
 
 # Topology_Density = SOLN.AddTopologyDensity()
-Topology_Density = DataModel.GetObjectsByName("Topology Density")[0]
+topology_density = DataModel.GetObjectsByName("Topology Density")[0]
 
 # %%
 # Solve: shape Optimization Simulation
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-SOLN.Solve(True)
-STAT_SHAPE_SS = SOLN.Status
+topo_solution.Solve(True)
+topo_solution_status = topo_solution.Status
 
 # sphinx_gallery_start_ignore
-assert str(STAT_STRUC_SS) == "Done", "Solution status is not 'Done'"
+assert str(topo_solution_status) == "Done", "Solution status is not 'Done'"
 # sphinx_gallery_end_ignore
 
 # %%
-# Messages
-# ~~~~~~~~
+# Show messages
+# ~~~~~~~~~~~~~
 
-Messages = ExtAPI.Application.Messages
-if Messages:
-    for message in Messages:
-        print(f"[{message.Severity}] {message.DisplayString}")
-else:
-    print("No [Info]/[Warning]/[Error] Messages")
+# Print all messages from Mechanical
+app.messages.show()
 
 # %%
 # Results
 # ~~~~~~~
 # Topology Density
 
-Tree.Activate([Topology_Density])
-Graphics.Camera.SetFit()
-Graphics.ExportImage(
-    os.path.join(cwd, "topology_density.png"), image_export_format, settings_720p
+app.Tree.Activate([topology_density])
+camera.SetFit()
+graphics.ExportImage(
+    str(output_path / "topology_density.png"), image_export_format, settings_720p
 )
 display_image("topology_density.png")
 
@@ -306,8 +363,11 @@ display_image("topology_density.png")
 # ~~~~~~~
 # Save project
 
-app.save(os.path.join(cwd, "shape-optimization.mechdat"))
-app.new()
+mechdat_file = output_path / "shape-optimization.mechdat"
+app.save(str(mechdat_file))
+
+# Close the app
+app.close()
 
 # delete example file
 delete_downloads()
